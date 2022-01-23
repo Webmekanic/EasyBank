06927196fcb2f765155d78bd183724a0a0a75f96
aniebodavid <aniebovictor001@gmail.com>
2022-01-23 08:48:04 -0800

    update on Hamburger Menu

--- app.js
+++ app.js
@@ -5,23 +5,11 @@ console.log(hamburger)
 hamburger.addEventListener("click", openMenu)
 
 function openMenu() {
-  console.log("clicked....")
   menuCollection.classList.toggle("active")
 
-  if (menuCollection.classList.contains("header__nav-active")) {
-    document.querySelector(".yourAnotherImg").src =
-      "some/path/to/anotherImg.svg"
+  if (menuCollection.classList.contains("active")) {
+    hamburger.src = "./images/icon-close.svg"
   } else {
-    document.querySelector(".yourBrandLogoImg").src =
-      "some/path/to/yourLogo.svg"
+    hamburger.src = "./images/icon-hamburger.svg"
   }
-
-  // nav.classList.toggle("active")
-  // btn.classList.toggle("active")
-
-  // if (nav.classList.contains("active")) {
-  //   btn.textContent = "X"
-  // } else {
-  //   btn.textContent = "☰"
-  // }
 }
